281b109b5138bf398fff5fe1a37ac606bb360e95
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-27 21:33:54 -0300

    :lipstick: feat: change style of checked card

--- src/components/FormElements/PlanCard/PlanCard.jsx
+++ src/components/FormElements/PlanCard/PlanCard.jsx
@@ -2,11 +2,14 @@ import "./PlanCard.css";
 
 function PlanCard({ photo, title, price }) {
   return (
-    <div className="card">
-      <img src={photo} alt="Plan Icon" />
-      <h3 className="plan-title">{title}</h3>
-      <p className="plan-price">${price}/mo</p>
-    </div>
+    <label>
+      <div className="card">
+        <input type="checkbox" />
+        <img src={photo} alt="Plan Icon" />
+        <h3 className="plan-title">{title}</h3>
+        <p className="plan-price">${price}/mo</p>
+      </div>
+    </label>
   );
 }
 
