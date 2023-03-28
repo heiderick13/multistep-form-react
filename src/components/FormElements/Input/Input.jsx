f9b7ea98a57eb0b8a6e40fcfa2876b18aaaad4e8
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-03-28 15:41:55 -0300

    :lipstick: style: update personal inputs

--- src/components/FormElements/Input/Input.jsx
+++ src/components/FormElements/Input/Input.jsx
@@ -1,10 +1,17 @@
 import "./Input.css";
 
-function Input({ id, name, label, type, placeHolder }) {
+function Input({ label, type, placeHolder }) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} placeholder={placeHolder} required />
+      <label>
+        {label}
+        <input
+          className="personal-input"
+          type={type}
+          placeholder={placeHolder}
+          required
+        />
+      </label>
     </>
   );
 }
